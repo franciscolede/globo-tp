10ddc42cfbdcc152b3cb7a4e263f3215a3f31c4c
franciscolede <franciscolede01@gmail.com>
2023-06-13 21:04:19 -0300

    Errores remarcados y no se puede ingresar espacios solos

--- scriptVue.js
+++ scriptVue.js
@@ -2,6 +2,8 @@ const app = Vue.createApp({
     data() {
       return {
 
+        razon: "",
+
         btnSeguir: "Seguir",
         btnSeguirColor: true,
 
@@ -54,88 +56,93 @@ const app = Vue.createApp({
 methods:{
         submitUser() {
             if (this.username !== ""){
-                this.vision.error = false
+                this.vision.error = false;
         
-                this.vision.userForm = false
+                this.vision.userForm = false;
                 
-                this.vision.user = true
-                this.usernameCheck = true
+                this.vision.user = true;
+                this.usernameCheck = true;
+                this.razon = "";
             }
             else{
-                
-                this.vision.error = true
-                inputUser.focus()
+                this.razon = "acceder";
+                this.vision.error = true;
+                inputUser.focus();
             }
         },
         
         follow() {
-            if (this.usernameCheck == true) {   
+            if (this.usernameCheck === true) {   
                 
-                if (this.btnSeguir == "Seguir"){
-                    this.btnSeguir = "Dejar de seguir"
+                if (this.btnSeguir === "Seguir"){
+                    this.btnSeguir = "Dejar de seguir";
 
-                    this.btnSeguirColor = false
+                    this.btnSeguirColor = false;
                 }
-                else if (this.btnSeguir == "Dejar de seguir"){
-                    this.btnSeguir = "Seguir"
+                else if (this.btnSeguir === "Dejar de seguir"){
+                    this.btnSeguir = "Seguir";
 
-                    this.btnSeguirColor = true
+                    this.btnSeguirColor = true;
                 }
+                this.razon = "";
             }
             else{
-                inputUser.focus()
-                this.vision.error = true
+                this.razon = "seguir el perfil";
+                inputUser.focus();
+                this.vision.error = true;
             }
         },
 
         like(){
-            if(this.usernameCheck == true){
+            if(this.usernameCheck === true){
 
-                if (this.btnLikeColor == true){
-                    this.likes += 1
-                    this.btnLikeColor = false
-                    this.btnLike = "No me gusta"
+                if (this.btnLikeColor === true){
+                    this.likes += 1;
+                    this.btnLikeColor = false;
+                    this.btnLike = "No me gusta";
                 }
                 else {
-                    this.likes -= 1
-                    this.btnLikeColor = true
-                    this.btnLike = "Me gusta"
+                    this.likes -= 1;
+                    this.btnLikeColor = true;
+                    this.btnLike = "Me gusta";
                 }
+                this.razon = "";
             }
             else{
-                inputUser.focus()
-                this.vision.error = true
+                this.razon = "dar like";
+                inputUser.focus();
+                this.vision.error = true;
             }
         },
 
         coment(){
-            if(this.usernameCheck == true){
-                if (this.comentText !== ""){
-                    this.comentArray.push(this.comentText)
-                    this.comentText = ""
-                    this.vision.comentError = false
-                    
-                }
-                else{
-                    this.vision.comentError = true
-                }
+            if(this.usernameCheck === true){
+                if (this.comentText.trim() !== ""){ 
+                    this.comentArray.push(this.comentText);
+                    this.comentText = "";
+                    this.vision.comentError = false;
+                  } else {
+                    this.vision.comentError = true;
+                  }
+                  this.razon = "";
             }
             else{
-                inputUser.focus()
-                this.vision.error = true
+                this.razon = "comentar";
+                inputUser.focus();
+                this.vision.error = true;
             }
         },
 
         eliminar(index){
-            this.comentArray.splice(index, 1)
+            this.comentArray.splice(index, 1);
         },
 
         modoOscuroButton(){
-            if(this.modoOscuro == false){
-                this.modoOscuro = true
+            if(this.modoOscuro === false){
+                this.modoOscuro = true;
             }
             else{
-                this.modoOscuro = false
+                this.modoOscuro = false;
             }
         },
 
